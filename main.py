274ed599f0758d1eed101e52c0ad2051b03929d3
Ming Creekmore <mcreekmore480@gmail.com>
2023-03-11 20:37:14 -0500

    Added comments

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
+"""
+Ming Creekmore
+Bioinformatics Languages
+11 March 2023
+
+Program that reads in data from covid19za_provincial_cumulative_timeline_confirmed.csv
+to perform exponential and cubic regressions on the increase of COVID-19 cases in different
+African regions from the time period 5 March 2020 to 9 July 2020
+Regions and their regressions are outputted into a txt document
+Charts of the regressions are saved as PDF files
+A line chart of all regions' COVID-19 data is also saved as a PDF file
+"""
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy
@@ -7,6 +19,16 @@ from scipy import stats
 
 
 def read_data(filename):
+    """
+    Reads the datafile covid19za_provincial_cumulative_timeline_confirmed.csv and puts it into
+    data structures
+    :Preconditions: Any other datafile will probably cause errors if the datafile is not formatted
+        the same
+    :param filename:
+    :return: numdays (the date list but reformatted as the number of days since the first entry)
+        datalists (dictionary of the regions to their respective data, as well as date)
+    """
+
     turn_to_int = lambda x: 0 if x == "" else int(x)
     datalists = {"date": list(), "EC": list(), "FS": list(), "GP": list(), "KZN": list(), "LP": list(),
                  "MP": list(), "NC": list(), "NW": list(), "WC": list(), "unknown": list()}#, "total": list()}
@@ -39,6 +61,14 @@ def read_data(filename):
 
 
 def plot_data(datalists):
+    """
+    Takes datalists and plots the cumulative data as a line chart, where date serves as the
+    x-axis and the other entries are different y-axis data.
+    This chart is saved as a PDF
+    :param datalists: dictionary of data to plot
+    :return: None
+    """
+
     df = pd.DataFrame(datalists)
     df.head()
     df.plot(x="date", title="Line Chart of COVID-19 Cases in Various African Regions in 2020")
@@ -49,6 +79,14 @@ def plot_data(datalists):
 
 
 def exp_regression(ydata, numdays, region):
+    """
+    Performs an exponential regression given the y-axis data and the x-axis data
+    Plots the scatter plot and the regression line
+    :param ydata:
+    :param numdays: the x-axis data for the chart/regression
+    :param region: Used for naming of the charts
+    :return: the exponential regression equation as a string
+    """
 
     # linearlize data by taking the log
     take_log = lambda t: 0 if t == 0 else math.log10(t)
@@ -71,6 +109,15 @@ def exp_regression(ydata, numdays, region):
 
 
 def poly_regression(ydata, numdays, region):
+    """
+    Performs a cubic regression given the y-axis data and the x-axis data
+    Plots the scatter plot and the regression line
+    :param ydata:
+    :param numdays: the x-axis data for the chart/regression
+    :param region: Used for naming of the charts
+    :return: the cubic regression equation as a string
+    """
+
     get_fit_str = lambda t: "{0:.3f}".format(t) if t < 0 else "+ " + "{0:.3f}".format(t)
 
     fit = numpy.poly1d(numpy.polyfit(numdays, ydata, 3))
@@ -88,6 +135,15 @@ def poly_regression(ydata, numdays, region):
 
 
 def main():
+    """
+    Makes a new directory "Charts" if it doesn't already exist to place all charts and txt
+    information file
+    Reads COVID-19 African case data, graphs a line chart of all region data,
+    performs exponential and cubic regressions on all region data and writes the
+    information to a file. All regression charts and the consolidated chart are saved as PDFs.
+    :return: None
+    """
+
     if not os.path.exists("./Charts"):
         os.mkdir("./Charts")
     file = "./archive/covid19za_provincial_cumulative_timeline_confirmed.csv"
